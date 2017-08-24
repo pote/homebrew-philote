5fef2772267450ee14188ba8104452ec99bc0e42
pote <poteland@gmail.com>
2017-08-24 02:54:23 -0300

    Updates jane

--- jane.rb
+++ jane.rb
@@ -1,8 +1,8 @@
 class Jane < Formula
   desc "A test CLI chat that uses Philote as it's backend."
   homepage "https://github.com/pote/jane"
-  url "https://github.com/pote/jane/archive/v0.1.2.tar.gz"
-  sha256 "b12fd11b3f105b122e7e6365f84c0a6e53e83a39895c52d584156114061010c0"
+  url "https://github.com/pote/jane/archive/v0.1.3.tar.gz"
+  sha256 "bb3a96d048a51eeea97832672e6b2f9d0d2ed8043f856ed68c60574547d41f83"
 
   depends_on "go"
   depends_on "gpm"
